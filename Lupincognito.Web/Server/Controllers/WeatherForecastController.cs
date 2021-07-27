d9949337fdf55b56420196d1ef89786f08f4fb54
James Schafer <james@jschafer.net>
2021-07-27 13:20:04 -0400

    Code Cleanup and add Fluxor and Nerdbank

--- Lupincognito.Web/Server/Controllers/WeatherForecastController.cs
+++ Lupincognito.Web/Server/Controllers/WeatherForecastController.cs
@@ -1,10 +1,9 @@
-﻿using Lupincognito.Web.Shared;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Lupincognito.Web.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Lupincognito.Web.Server.Controllers
 {
--- Lupincognito.Web/Shared/WeatherForecast.cs
+++ Lupincognito.Web/Shared/WeatherForecast.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Lupincognito.Web.Shared
 {
