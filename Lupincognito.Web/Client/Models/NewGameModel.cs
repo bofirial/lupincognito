52badab29a6f4290df9260a15df34348ad739d39
James Schafer <james@jschafer.net>
2021-07-31 22:56:49 -0400

    Remove PlayerName from the NewGame Screen

--- Lupincognito.Web/Client/Models/NewGameModel.cs
+++ Lupincognito.Web/Client/Models/NewGameModel.cs
@@ -7,9 +7,5 @@ namespace Lupincognito.Web.Client.Models
         [Required]
         [StringLength(25, ErrorMessage = "Game Name must be less than 25 characters.")]
         public string GameName { get; set; }
-
-        [Required]
-        [StringLength(25, ErrorMessage = "Player Name must be less than 25 characters.")]
-        public string PlayerName { get; set; }
     }
 }
